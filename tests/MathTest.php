d4957f4c6546e3efe3f53be50c094f5e2ddd5d61
Vincent Klaiber <vincent@doubledip.se>
2025-11-02 08:10:33 +0100

    chore: update tests to use PHPUnit 10+ attribute syntax

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -14,11 +14,12 @@ namespace Sqids\Tests;
 use Sqids\Math\BCMath;
 use Sqids\Math\Gmp;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use RuntimeException;
 
 class MathTest extends TestCase
 {
-    public static function mathProvider()
+    public static function mathProvider(): array
     {
         $providerCases = [];
         if (extension_loaded('gmp')) {
@@ -36,46 +37,46 @@ class MathTest extends TestCase
         throw new RuntimeException('Missing math extension for Sqids, install either bcmath or gmp.');
     }
 
-    /** @dataProvider mathProvider */
-    public function testAdd($math)
+    #[DataProvider('mathProvider')]
+    public function testAdd($math): void
     {
         $this->assertEquals($math->get(3), $math->add(1, 2));
     }
 
-    /** @dataProvider mathProvider */
-    public function testMultiply($math)
+    #[DataProvider('mathProvider')]
+    public function testMultiply($math): void
     {
         $this->assertEquals($math->get(12), $math->multiply(2, 6));
     }
 
-    /** @dataProvider mathProvider */
-    public function testDivide($math)
+    #[DataProvider('mathProvider')]
+    public function testDivide($math): void
     {
         $this->assertEquals($math->get(2), $math->divide(4, 2));
     }
 
-    /** @dataProvider mathProvider */
-    public function testGreaterThan($math)
+    #[DataProvider('mathProvider')]
+    public function testGreaterThan($math): void
     {
         $this->assertTrue($math->greaterThan('18446744073709551615', '9223372036854775807'));
         $this->assertFalse($math->greaterThan('9223372036854775807', '18446744073709551615'));
         $this->assertFalse($math->greaterThan('9223372036854775807', '9223372036854775807'));
     }
 
-    /** @dataProvider mathProvider */
-    public function testMod($math)
+    #[DataProvider('mathProvider')]
+    public function testMod($math): void
     {
         $this->assertEquals($math->get(15), $math->mod('18446744073709551615', '100'));
     }
 
-    /** @dataProvider mathProvider */
-    public function testIntval($math)
+    #[DataProvider('mathProvider')]
+    public function testIntval($math): void
     {
         $this->assertSame(9223372036854775807, $math->intval('9223372036854775807'));
     }
 
-    /** @dataProvider mathProvider */
-    public function testStrval($math)
+    #[DataProvider('mathProvider')]
+    public function testStrval($math): void
     {
         $this->assertSame('18446744073709551615', $math->strval($math->add('0', '18446744073709551615')));
     }
